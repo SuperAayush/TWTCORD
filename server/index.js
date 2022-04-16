7d9242d2970a48242264d7fdf471dc378d26472a
Aayush <aayush5emails@gmail.com>
2022-04-17 01:43:52 +0530

    feat:database connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 import express from "express";
-
+import connection from "./database/DB.js";
+import Router from "./Routes/Routes.js";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const URL = process.env.AZURE_URL;
 
 const PORT = 8000;
 
+app.use("/", Router);
+
 app.listen(PORT, () => console.log("Server is running"));
+
+connection(URL);
