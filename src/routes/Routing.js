f1fe1fd3342af627768b452b38e43fe5f0164c17
Suman <60940736+imSmaity@users.noreply.github.com>
2022-04-27 13:45:47 +0530

    Root path redirect to main page and create not found page

--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -1,16 +1,28 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import ServerBar from '../discord/Navbar/ServerBar'
 import Checking from '../middlewares/Checking'
 import News from '../News/News'
+
+function Redirect({to}){
+  const navigate=useNavigate()
+  useEffect(()=>{
+    navigate(to)
+  },[navigate,to])
+
+  return;
+}
 
 function Routing() {
   return (
     <>
       <Routes>
+          <Route path="/" element={<Redirect to={'/news'}/>} />
           <Route path="/news" exact element={<News />} />
           <Route path="/checking" exact element={<Checking />} />
           <Route path="/serverbar" exact element={<ServerBar />} />
+          <Route path="*" exact element={<h1>PAGE NOT FOUND</h1>} />
       </Routes>
     </>
   )
